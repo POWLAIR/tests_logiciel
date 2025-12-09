91de91fe466ea82339a3339a2930bd75cdc3a2f5
PowlAIR <paul.claverie@live.fr>
2025-12-09 11:48:59 +0100

    🔵 REFACTOR: [1.1] Add strict types and documentation

--- tdd/src/Laboratory.php
+++ tdd/src/Laboratory.php
@@ -1,11 +1,24 @@
 <?php
 
+declare(strict_types=1);
+
 namespace TDD;
 
+/**
+ * Laboratory manages substances, reactions and products.
+ */
 class Laboratory
 {
+    /** @var array<string, float> */
+    private array $stock = [];
+
+    /**
+     * @param array<int, string> $substances List of known substance names
+     */
     public function __construct(array $substances)
     {
-        // Minimal implementation to pass the test
+        foreach ($substances as $substance) {
+            $this->stock[$substance] = 0.0;
+        }
     }
 }
